d03fa47299709ee6e88cd6c671fdc2357ffbd094
neizer <dmr85@i.ua>
2019-05-17 16:24:52 +0300

    updated app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from module.accidentdata import AccidentData
-import time
 
 app = Flask(__name__)
 
@@ -25,14 +24,13 @@ def analyze():
         content_str = '<p><b>Error! Please, enter proper value!</b></p>'
         graphics = start_str + content_str + button_str + end_str
 
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    with open("templates/analysis"+timestr+".html", "w") as f:
+    with open("templates/analysis.html", "w") as f:
         begin_str = '{% extends "layout.html" %}{% block body %}'
         end_str = '{% endblock %}'
         body_str = f'<div class="col-md-8 col-md-offset-2" style="border-radius:25px;background:white;height:auto;padding:20px 40px 20px 60px;">{graphics}</div>'
         page_str = begin_str + body_str + end_str
         f.write(page_str)
-    return render_template("analysis"+timestr+".html")
+    return render_template("analysis.html")
 
 
 if __name__ == '__main__':

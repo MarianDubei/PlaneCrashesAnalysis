6395acd4f618715a3e639da4bb25e3c1f47a96e3
neizer <dmr85@i.ua>
2019-05-17 13:41:01 +0300

    updated app.py

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ def analyze():
     criteria_category = request.form.get('button')
     criteria_obj = request.form.get('criteria')
     button_str = '<a href="/"><button type="button" class="btn btn-primary btn-lg">Return to main page</button></a>'
-    try:
-        accidents = AccidentData(criteria_category, criteria_obj)
-        accidents.get_data()
-        graphics = accidents.show_infographics() + button_str
-    except:
-        start_str = '<div class="row mb-3"><div class="col-md-12">'
-        end_str = '</div></div>'
-        content_str = '<p><b>Error! Please, enter proper value!</b></p>'
-        graphics = start_str + content_str + button_str + end_str
+    # try:
+    accidents = AccidentData(criteria_category, criteria_obj)
+    accidents.get_data()
+    graphics = accidents.show_infographics() + button_str
+    # except:
+    #     start_str = '<div class="row mb-3"><div class="col-md-12">'
+    #     end_str = '</div></div>'
+    #     content_str = '<p><b>Error! Please, enter proper value!</b></p>'
+    #     graphics = start_str + content_str + button_str + end_str
 
-    with open("templates/analysis.html", "w") as f:
+    with open("../templates/analysis.html", "w") as f:
         begin_str = '{% extends "layout.html" %}{% block body %}'
         end_str = '{% endblock %}'
         body_str = f'<div class="col-md-8 col-md-offset-2" style="border-radius:25px;background:white;height:auto;padding:20px 40px 20px 60px;">{graphics}</div>'

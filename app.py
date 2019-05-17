b8377f5bcb1d5481347c84c50bbee6303c41a298
neizer <dmr85@i.ua>
2019-05-17 15:48:52 +0300

    updated app

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def analyze():
         content_str = '<p><b>Error! Please, enter proper value!</b></p>'
         graphics = start_str + content_str + button_str + end_str
 
-    with open("../templates/analysis.html", "w") as f:
+    with open("templates/analysis.html", "w") as f:
         begin_str = '{% extends "layout.html" %}{% block body %}'
         end_str = '{% endblock %}'
         body_str = f'<div class="col-md-8 col-md-offset-2" style="border-radius:25px;background:white;height:auto;padding:20px 40px 20px 60px;">{graphics}</div>'
